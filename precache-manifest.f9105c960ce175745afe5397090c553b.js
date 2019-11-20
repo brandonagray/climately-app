73cfe887aa734de8f4f4f5070e0d6c8286e8dcd3
Brandon Gray <grayba@gmail.com>
2019-11-20 14:13:29 +1300

    Updates

---
+++ precache-manifest.f9105c960ce175745afe5397090c553b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158a61c49bdbbc17efa7f80b6bc4709f",
+    "url": "/climately-app/index.html"
+  },
+  {
+    "revision": "cd20be0514fb8ded549f",
+    "url": "/climately-app/static/css/2.9d04c5f8.chunk.css"
+  },
+  {
+    "revision": "21daef6759c769219cfc",
+    "url": "/climately-app/static/css/main.a3bbc1ee.chunk.css"
+  },
+  {
+    "revision": "cd20be0514fb8ded549f",
+    "url": "/climately-app/static/js/2.43c5e286.chunk.js"
+  },
+  {
+    "revision": "21daef6759c769219cfc",
+    "url": "/climately-app/static/js/main.b2318e7b.chunk.js"
+  },
+  {
+    "revision": "5d8892af6d5ca89e06de",
+    "url": "/climately-app/static/js/runtime-main.e0d48f58.js"
+  }
+]);
